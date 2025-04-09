26d306e1169240bbba08f60847f86597985c9507
emak3 <nana_kame@icloud.com>
2025-04-09 23:55:05 +0900

    aaa

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps : [{
-      name: 'discord-bot',
+      name: 'Claude3-bot',
       script: 'index.js',
       cwd: 'C:/Users/nana_/Documents/Program/Claude3-bot', // Botのパス
       watch: false,
